236631bb211225737864ee26b627a27509482491
Jose9920 <jose.1701722372@ucaldas.edu.co>
2022-06-07 19:22:49 -0500

    implementacion de la funcionalidad crear sitio backend

--- src/config/MongoDB.py
+++ src/config/MongoDB.py
@@ -1,7 +1,5 @@
 from pymongo import MongoClient
 from decouple import config
 #mongodb+srv://JoseManuel:*******@ubicateuc.c3jbt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-
-#url = config("mongodb+srv://JoseManuel:t4IUfatJA9XyyyvD@ubicateuc.c3jbt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 coneccion = MongoClient("ConectionString")
 db = coneccion.UbicateUC
