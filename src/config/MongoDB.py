690560f1a1ddc1ae3c0fc0d189eda8fc3352ca47
Jose9920 <jose.1701722372@ucaldas.edu.co>
2022-06-07 19:18:25 -0500

    implementacion de creacion de sitios backend

--- src/config/MongoDB.py
+++ src/config/MongoDB.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 from decouple import config
+#mongodb+srv://JoseManuel:*******@ubicateuc.c3jbt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
-url = config("MONGODB_URL")
-coneccion = MongoClient(url)
+#url = config("mongodb+srv://JoseManuel:t4IUfatJA9XyyyvD@ubicateuc.c3jbt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+coneccion = MongoClient("ConectionString")
 db = coneccion.UbicateUC
