cc92a80dc2ffb77172d45595d4438566497c52eb
Cristian David Escobar <cescobar@truora.com>
2022-09-20 16:51:35 -0500

    provitional fix to connection db

--- src/config/MongoDB.py
+++ src/config/MongoDB.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 from decouple import config
 
-url = config("MONGODB_URL")
-coneccion = MongoClient(url)
+#url = config("MONGODB_URL")
+coneccion = MongoClient(
+    "mongodb+srv://ubicateuc:KqCV9CYcZAbR46YZ@ubicateuc.c3jbt.mongodb.net/test")
 db = coneccion.UbicateUC
